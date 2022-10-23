031c8e0d037f64a5020d4ed0674e3497bcc2689c
CristopherGSR <crisgs608@gmail.com>
2022-10-23 18:34:07 -0500

    Rutas, modulos

--- CapanioStore/store/urls.py
+++ CapanioStore/store/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.store_view, name='Index'),
+    path('contact_us/',views.contact_us, name='contact_us'),
+    path('menu/', views.menu, name='menu'),
+    path('reservation/',views.reservation, name='reservation'),
+    path('about_us/',views.about_us, name='about_us')
 ]
--- CapanioStore/store/views.py
+++ CapanioStore/store/views.py
@@ -1,6 +1,20 @@
+from ast import Return
+import re
 from django.shortcuts import render
 
 # Create your views here.
 
 def store_view(request):
     return render(request, 'index.html')
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+def reservation(request):
+    return render(request, 'reservation.html')
