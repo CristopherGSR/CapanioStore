8f1c65f0dfa4fd3e67443a28338cc38f08d9f627
CristopherGSR <crisgs608@gmail.com>
2022-11-08 20:15:17 -0500

    Models
    Se agrego modelos y actualizacion de productos.

--- CapanioStore/store/models.py
+++ CapanioStore/store/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.deletion import SET_NULL
 
 # Create your models here.
+# makemigrations
+# migrate
+
+#CREANDO CLASE CLIENTE
+class Cliente(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=50, null=True)
+    correo = models.CharField(max_length=50)
+    def __str__(self):
+        return self.nombre
+
+#CREANDO CLASE CATEGORIA
+class Categoria(models.Model):
+    codigo = models.CharField(max_length=3, primary_key=True)
+    nombre = models.CharField(max_length=40)
+    def __str__(self):
+        return self.nombre
+
+#CREANDO CLASE PRODUCTOS
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    categoria = models.ForeignKey(Categoria, null=False, blank=False, on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=7, decimal_places=2)
+    descripcion = models.CharField(max_length=200)
+    imagen = models.ImageField(null=True, blank=True)
+    def __str__(self):
+        return self.nombre
+    @property
+    def imagenURL(self):
+        try:
+            url = self.imagen.url
+        except:
+            url = ' '
+        return url
--- CapanioStore/store/views.py
+++ CapanioStore/store/views.py
@@ -1,6 +1,7 @@
 from ast import Return
 import re
 from django.shortcuts import render
+from store.models import Producto
 
 # Create your views here.
 
@@ -11,7 +12,12 @@ def contact_us(request):
     return render(request, 'contact_us.html')
 
 def menu(request):
-    return render(request, 'menu.html')
+    productos_segundos = Producto.objects.filter(categoria="001")
+    productos_caldos = Producto.objects.filter(categoria="004")
+    productos_postres = Producto.objects.filter(categoria="003")
+    productos_bebidas = Producto.objects.filter(categoria="002")
+    context = {'productos':productos_segundos, 'productos2':productos_caldos, 'productos3':productos_postres, 'productos4':productos_bebidas}
+    return render(request, 'menu.html', context)
 
 def about_us(request):
     return render(request, 'about_us.html')
